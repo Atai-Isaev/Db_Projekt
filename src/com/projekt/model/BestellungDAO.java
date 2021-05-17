204f9083518ce317712adb26d0ae3abc8773fde0
Atai-Isaev <Atai.Isaev.kx7@fh-zwickau.de>
2021-05-17 11:52:27 +0200

    show table Bestellung from DB

--- src/com/projekt/model/BestellungDAO.java
+++ src/com/projekt/model/BestellungDAO.java
@@ -1,81 +1,51 @@
 package com.projekt.model;
 
-import java.util.Date;
+import com.projekt.connectivity.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BestellungDAO {
-    private int BestellungNr;
-    private int KundeNr;
-    private int Bestellstatus;
-    private Date Bestelldatum;
-    private Date Bedarfsdatum;
-    private Date Versanddatum;
-    private int GeschäftNr;
-    private int MitarbeiterNr;
+    private Bestellung createBestellung(ResultSet rs) {
+        Bestellung b = new Bestellung();
 
-    public BestellungDAO() {
+        try {
+            b.setBestellungNr(rs.getInt("BestellungNr"));
+            b.setKundeNr(rs.getInt("KundeNr"));
+            b.setBestellstatus(rs.getInt("Bestellstatus"));
+            b.setBestelldatum(rs.getDate("Bestelldatum"));
+            b.setBedarfsdatum(rs.getDate("Bedarfsdatum"));
+            b.setVersanddatum(rs.getDate("Versanddatum"));
+            b.setGeschäftNr(rs.getInt("GeschäftNr"));
+            b.setMitarbeiterNr(rs.getInt("MitarbeiterNr"));
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return b;
     }
 
-    public int getBestellungNr() {
-        return BestellungNr;
-    }
+    public List<Bestellung> getBestellungs() {
+        String sql = "Select * from Bestellung order by BestellungNr";
+        List<Bestellung> list = new ArrayList<>();
+        try {
+            DatabaseConnection databaseConnection = new DatabaseConnection();
+            Connection con = databaseConnection.getConnection();
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
 
-    public void setBestellungNr(int bestellungNr) {
-        BestellungNr = bestellungNr;
-    }
-
-    public int getKundeNr() {
-        return KundeNr;
-    }
-
-    public void setKundeNr(int kundeNr) {
-        KundeNr = kundeNr;
-    }
-
-    public int getBestellstatus() {
-        return Bestellstatus;
-    }
-
-    public void setBestellstatus(int bestellstatus) {
-        Bestellstatus = bestellstatus;
-    }
-
-    public Date getBestelldatum() {
-        return Bestelldatum;
-    }
-
-    public void setBestelldatum(Date bestelldatum) {
-        Bestelldatum = bestelldatum;
-    }
-
-    public Date getBedarfsdatum() {
-        return Bedarfsdatum;
-    }
-
-    public void setBedarfsdatum(Date bedarfsdatum) {
-        Bedarfsdatum = bedarfsdatum;
-    }
-
-    public Date getVersanddatum() {
-        return Versanddatum;
-    }
-
-    public void setVersanddatum(Date versanddatum) {
-        Versanddatum = versanddatum;
-    }
-
-    public int getGeschäftNr() {
-        return GeschäftNr;
-    }
-
-    public void setGeschäftNr(int geschäftNr) {
-        GeschäftNr = geschäftNr;
-    }
-
-    public int getMitarbeiterNr() {
-        return MitarbeiterNr;
-    }
-
-    public void setMitarbeiterNr(int mitarbeiterNr) {
-        MitarbeiterNr = mitarbeiterNr;
+            while (rs.next()) {
+                Bestellung b = createBestellung(rs);
+                list.add(b);
+            }
+            rs.close();
+            con.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return list;
     }
 }
--- src/com/projekt/view/DbOverviewController.java
+++ src/com/projekt/view/DbOverviewController.java
@@ -9,6 +9,7 @@ import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.math.BigDecimal;
+import java.util.Date;
 
 public class DbOverviewController {
     private ArtikelDAO artikelDAO = new ArtikelDAO();
@@ -22,7 +23,7 @@ public class DbOverviewController {
     private MitarbeiterDAO mitarbeiterDAO = new MitarbeiterDAO();
 
     private ObservableList<Artikel> artikelObservableList = FXCollections.observableArrayList(artikelDAO.getArtikels());
-//    private ObservableList<Bestellung> bestellungObservableList = FXCollections.observableArrayList(bestellungDAO.getBestellungs());
+    private ObservableList<Bestellung> bestellungObservableList = FXCollections.observableArrayList(bestellungDAO.getBestellungs());
 //    private ObservableList<Bestellung_Artikel> bestellung_artikelObservableList = FXCollections.observableArrayList(bestellung_artikelDAO.getBestellungs());
 //    private ObservableList<Bestände> beständeObservableList = FXCollections.observableArrayList(beständeDAO.getBestellungs());
 //    private ObservableList<Geschäft> geschäftObservableList = FXCollections.observableArrayList(geschäftDAO.getBestellungs());
@@ -68,6 +69,25 @@ public class DbOverviewController {
     private TableColumn<Artikel, BigDecimal> artikelListenpreisColumn;
 
     @FXML
+    private TableView<Bestellung> bestellungTableView;
+    @FXML
+    private TableColumn<Bestellung, Integer> bestellungNrColumn;
+    @FXML
+    private TableColumn<Bestellung, Integer> bestellungKundeNrColumn;
+    @FXML
+    private TableColumn<Bestellung, Integer> bestellstatusColumn;
+    @FXML
+    private TableColumn<Bestellung, Date> bestelldatumColumn;
+    @FXML
+    private TableColumn<Bestellung, Date> bedarfsdatumColumn;
+    @FXML
+    private TableColumn<Bestellung, Date> versanddatumColumn;
+    @FXML
+    private TableColumn<Bestellung, Integer> bestellungGeschäftNrColumn;
+    @FXML
+    private TableColumn<Bestellung, Integer> bestellungMitarbeiterNrColumn;
+
+    @FXML
     private ButtonBar buttonBar;
 
     @FXML
@@ -82,19 +102,33 @@ public class DbOverviewController {
     private final String[] artikelPropertyName = {"ArtikelNr",
             "ArtikelName", "HerstellerNr", "KategorieNr", "Modelljahr", "Listenpreis"};
 
+    private final String[] bestellungPropertyName = {"BestellungNr",
+            "KundeNr", "Bestellstatus", "Bestelldatum", "Bedarfsdatum",
+            "Versanddatum", "GeschäftNr", "MitarbeiterNr"};
+
+
     public DbOverviewController() {
     }
 
     @FXML
     private void initialize() {
         artikelTableView.setItems(this.artikelObservableList);
-
         artikelNrColumn.setCellValueFactory(new PropertyValueFactory<>(artikelPropertyName[0]));
         artikelNameColumn.setCellValueFactory(new PropertyValueFactory<>(artikelPropertyName[1]));
         artikelHerstellerNrColumn.setCellValueFactory(new PropertyValueFactory<>(artikelPropertyName[2]));
         artikelKategorieNrColumn.setCellValueFactory(new PropertyValueFactory<>(artikelPropertyName[3]));
         artikelModelljahrColumn.setCellValueFactory(new PropertyValueFactory<>(artikelPropertyName[4]));
         artikelListenpreisColumn.setCellValueFactory(new PropertyValueFactory<>(artikelPropertyName[5]));
+
+        bestellungTableView.setItems(this.bestellungObservableList);
+        bestellungNrColumn.setCellValueFactory(new PropertyValueFactory<>(bestellungPropertyName[0]));
+        bestellungKundeNrColumn.setCellValueFactory(new PropertyValueFactory<>(bestellungPropertyName[1]));
+        bestellstatusColumn.setCellValueFactory(new PropertyValueFactory<>(bestellungPropertyName[2]));
+        bestelldatumColumn.setCellValueFactory(new PropertyValueFactory<>(bestellungPropertyName[3]));
+        bedarfsdatumColumn.setCellValueFactory(new PropertyValueFactory<>(bestellungPropertyName[4]));
+        versanddatumColumn.setCellValueFactory(new PropertyValueFactory<>(bestellungPropertyName[5]));
+        bestellungGeschäftNrColumn.setCellValueFactory(new PropertyValueFactory<>(bestellungPropertyName[6]));
+        bestellungMitarbeiterNrColumn.setCellValueFactory(new PropertyValueFactory<>(bestellungPropertyName[7]));
     }
 
     public void setMain(Main main) {
@@ -109,7 +143,9 @@ public class DbOverviewController {
     @FXML
     private void handleUpdateAllData() {
         artikelObservableList = FXCollections.observableArrayList(artikelDAO.getArtikels());
-
         artikelTableView.setItems(artikelObservableList);
+
+        bestellungObservableList = FXCollections.observableArrayList(bestellungDAO.getBestellungs());
+        bestellungTableView.setItems(bestellungObservableList);
     }
 }
